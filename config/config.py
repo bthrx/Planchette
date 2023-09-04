cbcf679e00cff87c1b5a2d769ab7d444d06edcac
bthrx <bthrx@bthrx.com>
2023-09-04 14:35:34 -0400

    v.01 release

--- config/config.py
+++ config/config.py
@@ -1,7 +1,6 @@
 import configparser
 
-config = configparser.ConfigParser()
-config.read('config/keys.ini')
-
 def get_api_key(service_name):
-    return config.get(service_name, 'api_key')
+    config = configparser.ConfigParser()
+    config.read('config/keys.ini')
+    return config.get(service_name, 'api_key').strip("'\"")
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import argparse
 import re
 import requests
+from openai_request.openai_debug import openai_request
 from caidocsv.caidocsv import parse_csv
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--caido-csv', help='Provide the path to a Caido requests and responses CSV file')
-
+parser.add_argument(
+    "--caido-csv", help="Provide the path to a Caido requests and responses CSV file"
+)
+parser.add_argument(
+    "--llm", choices=["openai"], default="openai", help="Select LLM to use."
+)
+parser.add_argument("--output", type=str, help="Output file path")
 args = parser.parse_args()
 
 if args.caido_csv:
@@ -13,9 +19,9 @@ if args.caido_csv:
     tld_url = "https://data.iana.org/TLD/tlds-alpha-by-domain.txt"
     tld_response = requests.get(tld_url)
     tlds = tld_response.text.lower().splitlines()
-    request_list = [element.decode('latin-1') for element in r_request]
-    response_list = [element.decode('latin-1') for element in r_response]
-    tld_regex = r'\b(' + '|'.join(tlds) + r')\b'
+    request_list = [element.decode("latin-1") for element in r_request]
+    response_list = [element.decode("latin-1") for element in r_response]
+    tld_regex = r"\b(" + "|".join(tlds) + r")\b"
     request_map = {}
     response_map = {}
     cleaned_requests = []
@@ -26,14 +32,16 @@ if args.caido_csv:
 
         for match in re.finditer(tld_regex, request, re.IGNORECASE):
             tld = match.group(0).lower()
-            domain_regex = r'(?<=\.)[a-zA-Z0-9-]+(?=\.' + tld + r')'
+            domain_regex = r"(?<=\.)[a-zA-Z0-9-]+(?=\." + tld + r")"
             for domain_match in re.finditer(domain_regex, request, re.IGNORECASE):
                 domain = domain_match.group(0).lower()
-                
+
                 if domain not in request_map:
-                    request_map[domain] = f'example{len(request_map)}'
+                    request_map[domain] = f"example{len(request_map)}"
                 clean_domain = request_map[domain]
-                new_request = new_request.replace(domain + '.' + tld, clean_domain + '.' + tld)
+                new_request = new_request.replace(
+                    domain + "." + tld, clean_domain + "." + tld
+                )
         cleaned_requests.append(new_request)
 
     for response in response_list:
@@ -41,15 +49,25 @@ if args.caido_csv:
 
         for match in re.finditer(tld_regex, response, re.IGNORECASE):
             tld = match.group(0).lower()
-            domain_regex = r'(?<=\.)[a-zA-Z0-9-]+(?=\.' + tld + r')'
+            domain_regex = r"(?<=\.)[a-zA-Z0-9-]+(?=\." + tld + r")"
             for domain_match in re.finditer(domain_regex, response, re.IGNORECASE):
                 domain = domain_match.group(0).lower()
-                
-                if domain not in response_map:
-                    response_map[domain] = f'example{len(response_map)}'
-                clean_domain = response_map[domain]
-                new_response = new_response.replace(domain + '.' + tld, clean_domain + '.' + tld)
-        cleaned_responses.append(new_response)
-    
 
-    
+                if domain not in response_map:
+                    response_map[domain] = f"example{len(response_map)}"
+                clean_domain = response_map[domain]
+                new_response = new_response.replace(
+                    domain + "." + tld, clean_domain + "." + tld
+                )
+        cleaned_response_headers = new_response.split('\r\n\r\n')[0]
+        cleaned_responses.append(cleaned_response_headers)
+
+if args.llm == "openai":
+    ai_messages = openai_request(cleaned_requests, cleaned_responses)
+
+#    if args.output:
+#        with open(args.output, "w") as f:
+#                f.write(ai_messages)
+#    else:
+#        for message in ai_messages:
+#            print(message)
